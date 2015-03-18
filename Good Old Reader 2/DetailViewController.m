216317c3259526545dd67e59687903d6ebd71651
András Somogyi <somogyi.andras@gmail.com>
2015-03-18 16:39:03 +0100

    New features and some bugfix Date, author, origin site displayed in article view. Article JSON passed to article view and prepared there. BUG FIXED: Exception when empty fields in JSON data

--- Good Old Reader 2/DetailViewController.m
+++ Good Old Reader 2/DetailViewController.m
@@ -15,13 +15,68 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self displayArticle];
+}
+
+- (void)displayArticle {
+    NSString *articleUpdateDateString;
+    NSString *articleTitle;
+    NSString *articleAuthor;
+    NSString *articleOrigin;
+    NSString *articleSummary;
     
-    // Do any additional setup after loading the view.
+    // Query article title if exists
+    if ([self.articleContainer objectForKey:@"title"] == [NSNull null] || [[self.articleContainer objectForKey:@"title"]  isEqual: @""]) {
+        articleTitle = @"No title";
+    }
+    else articleTitle = [self.articleContainer objectForKey:@"title"];
+    
+    
+    // Query article author if exists
+    if ([self.articleContainer objectForKey:@"author"] == [NSNull null] || [[self.articleContainer objectForKey:@"author"]  isEqual: @""]) {
+        articleAuthor = @"Anonymus";
+    }
+    else articleAuthor = [self.articleContainer objectForKey:@"author"];
+    
+    
+    // Query article update date if exists
+    if ([self.articleContainer objectForKey:@"updated"] == [NSNull null]) {
+        articleUpdateDateString = @"";
+    }
+    else {
+        NSDate *articleUpdateDate = [NSDate dateWithTimeIntervalSince1970:[[self.articleContainer objectForKey:@"updated"] doubleValue]];
+        NSDateFormatter *articleUpdateDateFormatter = [[NSDateFormatter alloc] init];
+        [articleUpdateDateFormatter setDateStyle:NSDateFormatterShortStyle];
+        [articleUpdateDateFormatter setTimeStyle:NSDateFormatterShortStyle];
+        articleUpdateDateString = [articleUpdateDateFormatter stringFromDate:articleUpdateDate];}
+    
+    
+    // Query origin site if exists
+    if ([[self.articleContainer objectForKey:@"origin"] objectForKey:@"title"] == nil || [[[self.articleContainer objectForKey:@"origin"] objectForKey:@"title"]  isEqual: @""]) {
+        articleOrigin = @"No site";
+        
+    }
+    else articleOrigin = [[self.articleContainer objectForKey:@"origin"] objectForKey:@"title"];
+    
+    // Query article text if exists
+    if ([[self.articleContainer objectForKey:@"summary"] objectForKey:@"content"] == [NSNull null] || [[[self.articleContainer objectForKey:@"summary"] objectForKey:@"content"]  isEqual: @""]) {
+        articleSummary = @"";
+    }
+    else articleSummary = [[self.articleContainer objectForKey:@"summary"] objectForKey:@"content"];
+    
     NSMutableString *preparedArticle = [NSMutableString stringWithString:@"<style type='text/css'>img { max-width: 100%; width: auto; height: auto;}body{font-family:helvetica; font-size:12px;}</style>"];
     [preparedArticle appendString:@"<h1>"];
-    [preparedArticle appendString:self.articleContainer[@"Title"]];
+    [preparedArticle appendString:articleTitle];
     [preparedArticle appendString:@"</h1>"];
-    [preparedArticle appendString:self.articleContainer[@"Content"]];
+    [preparedArticle appendString:@"<p><i>"];
+    [preparedArticle appendString:articleAuthor];
+    [preparedArticle appendString:@", "];
+    [preparedArticle appendString:articleOrigin];
+    [preparedArticle appendString:@", "];
+    [preparedArticle appendString:articleUpdateDateString];
+    [preparedArticle appendString:@"</i></p>"];
+    [preparedArticle appendString:articleSummary];
+    
     [self.articleDisplay loadHTMLString:preparedArticle baseURL:nil];
 }
 
