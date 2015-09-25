6f6344e0d3fc272da153d45e301126e1cc744346
Andr√°s Somogyi <somogyi.andras@gmail.com>
2015-09-25 16:02:55 +0200

    Toolbar actions added * Share button for  #62 * Keep unread #60 TBD * Button to open in Safari view controller #58

--- Good Old Reader 2/View Controllers/DetailViewController.m
+++ Good Old Reader 2/View Controllers/DetailViewController.m
@@ -10,8 +10,12 @@
 #import "ApiManager.h"
 #import "Article+HTMLRepresentation.h"
 
-@interface DetailViewController ()
+
+@interface DetailViewController () <SFSafariViewControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *articleDisplay;
+@property (weak, nonatomic) IBOutlet UIToolbar *detailViewToolBar;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *openInSafariBarButton;
+
 @end
 
 @implementation DetailViewController
@@ -26,6 +30,11 @@
     [self markAsReadOnServer];
 }
 
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
 #pragma mark - Actions
 - (void)displayArticle {
     [self.articleDisplay loadHTMLString:[Article HTMLRepresentation:self.articleContainer] baseURL:nil];
@@ -36,9 +45,20 @@
     [ApiManager markArticleRead:self.articleContainer.articleId withCompletion:nil withError:nil];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)shareAction:(id)sender {
+    NSString *message = @"Check out this arcticle!";
+    NSString *articleToShare = self.articleContainer.canonical;
+    NSArray *postItems = @[message, articleToShare];
+    
+    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:postItems applicationActivities:nil];
+    
+    [self presentViewController:activityVC animated:YES completion:nil];
+}
+
+- (IBAction)openInSafariAction:(id)sender {
+    SFSafariViewController *safariVC = [[SFSafariViewController alloc]initWithURL:[NSURL URLWithString:self.articleContainer.canonical] entersReaderIfAvailable:YES];
+    safariVC.delegate = self;
+    [self presentViewController:safariVC animated:NO completion:nil];
 }
 
 @end
